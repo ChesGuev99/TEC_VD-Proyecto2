a9bb58d73cb50f67832b1700b4627a8dbf4c4226
ChesGuev99 <anaguevarabonilla@gmail.com>
2020-11-13 19:14:29 -0600

    Uncomment

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from EjemploBuscador import ejemplohtml
 from Excel import lectura
 from ejemploSea import sea
 if __name__ == '__main__':
-    #ejemplohtml()
+    ejemplohtml()
     #sea()
     lectura()
