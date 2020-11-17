b435e574745aa60a1be93fc78da9b7530907b678
xalexedgex <39139089+xalexedgex@users.noreply.github.com>
2020-11-16 18:26:49 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from EjemploBuscador import ejemplohtml
 from Excel import lectura
 from ejemploSea import sea
+
 if __name__ == '__main__':
-    ejemplohtml()
-    #sea()
-    lectura()
+    lectura((buscarArchivo()))
