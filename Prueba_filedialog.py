0902fa3b7376d84123415742783309aec85f7e3f
xalexedgex <39139089+xalexedgex@users.noreply.github.com>
2020-11-16 18:24:50 -0600

    Update Prueba_filedialog.py

--- Prueba_filedialog.py
+++ Prueba_filedialog.py
@@ -1,6 +1,7 @@
 from tkinter import Tk 
 from tkinter.filedialog import askopenfilename
 
-Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-filename = askopenfilename(filetypes=[("Excel files", "*.xlsx .csv")]) # show an "Open" dialog box and return the path to the selected file
-print(filename) # Solo imprime el path. :)
+def buscarArchivo():
+	Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+	filename = askopenfilename(filetypes=[("Excel files", "*.xlsx .xls .csv")]) # show an "Open" dialog box and return the path to the selected file
+	return filename
